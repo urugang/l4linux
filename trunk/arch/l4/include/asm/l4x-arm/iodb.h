aba9687e9dff50d3589c82fd473858a94aae8202
l4check <l4check@0a9c79ab-7a64-452d-b398-066b00a4785d>
2010-11-09 08:48:39 +0000

    update: sync

--- trunk/arch/l4/include/asm/l4x-arm/iodb.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ASM_L4__L4X_ARM__IODB_H__
-#define __ASM_L4__L4X_ARM__IODB_H__
-
-static inline void l4x_iodb_free(struct task_struct *task)
-{
-}
-
-#endif /* ! __ASM_L4__L4X_ARM__IODB_H__ */
